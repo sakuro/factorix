41a94d35cc1dcefef2e8e110c03fc48bcc4e85c4
OZAWA Sakuro <10973+sakuro@users.noreply.github.com>
2025-02-24 15:16:21 +0900

    :computer: bundle gem factorix

---
+++ lib/factorix.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "factorix/version"
+
+module Factorix
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/factorix/version.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+module Factorix
+  VERSION = "0.1.0"
+end
