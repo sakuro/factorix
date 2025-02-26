259ad68696bef9cc6f42fc5fc6ba014f30d066aa
OZAWA Sakuro <10973+sakuro@users.noreply.github.com>
2025-02-26 12:20:52 +0900

    :inbox_tray: Add coverage (#5)
    * :gem: Add simplecov
    * :wrench: upload coverage as an artifact

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,11 @@
 # frozen_string_literal: true
 
+require "simplecov"
+SimpleCov.start do
+  enable_coverage :branch
+  add_filter %r{\A/spec/}
+end
+
 require "factorix"
 
 RSpec.configure do |config|
