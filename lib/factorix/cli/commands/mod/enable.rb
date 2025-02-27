3a0488d4f9d1d10f74319d8e6f816e2d8f6ce641
OZAWA Sakuro <10973+sakuro@users.noreply.github.com>
2025-02-27 17:51:49 +0900

    :hammer: Restructure enable/disable commands under mod namespace

---
+++ lib/factorix/cli/commands/mod/enable.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+require "dry/cli"
+
+module Factorix
+  class CLI
+    module Commands
+      module Mod
+        # Command for enabling a mod
+        class Enable < Dry::CLI::Command
+          desc "Enable a MOD"
+
+          argument :mod, required: true, desc: "The MOD to enable"
+          option :verbose, type: :boolean, default: false, desc: "Print more information"
+
+          # Enable a MOD
+          # @param mod [String] The MOD to enable
+          # @param options [Hash] The options for the command
+          # @option options [Boolean] :verbose Print more information
+          def call(mod:, **options)
+            puts "Enabling MOD: #{mod}" if options[:verbose]
+            list = Factorix::ModList.load
+            list.enable(Factorix::Mod[name: mod])
+            list.save
+          end
+        end
+      end
+    end
+  end
+end
